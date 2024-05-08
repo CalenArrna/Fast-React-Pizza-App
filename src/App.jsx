19a14cf8a4144d903b53e963c35f8da62e22dc9c
Nagy Csaba <nagycsaba@MacBook-Pro-16.local>
2024-05-08 23:18:33 +0200

    feat(features): added create order, form validation, api calls

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
-import CreateOrder from "./features/order/CreateOrder.jsx";
-import Order from "./features/order/Order.jsx";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder.jsx";
+import Order, { loader as orderLoader } from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import Error from "./ui/Error.jsx";
 
@@ -20,8 +22,17 @@ const router = createBrowserRouter([
         loader: menuLoader,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        errorElement: <Error />,
+        loader: orderLoader,
+      },
     ],
   },
 ]);
